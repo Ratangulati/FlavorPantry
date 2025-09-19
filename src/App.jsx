390d8b6bb80952b58d5ce6b3ebb7159380d3c914
Om Kadu <kaduom444@gmail.com>
2025-09-19 13:01:35 +0530

    integrated emailjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Home from './pages/Home';
 import Recipes from './pages/Recipes';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/recipes' element={<Recipes />} />
-    </Routes>
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/recipes' element={<Recipes />} />
+      </Routes>
+    </>
   );
 }
 
